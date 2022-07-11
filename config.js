f2391dd016c9e2b02792ac6c794a503cf1185e9c
Official-Rup <107984412+Official-Rup@users.noreply.github.com>
2022-07-11 08:22:31 +0530

    Update config.js
    Signed-off-by: Official-Rup <107984412+Official-Rup@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['918617845264','']
+global.premium = ['918617845264','919749388192']
+global.botname = 'ɪɴᴄ|🆁︎🆄︎🅿︎❤'
+global.ownername = '❥͜͡𝄟♥⃝•⩤⋆🆁︎🅤︎🅿︎⋆⩥࿐ཽ༵•❥͜͡★'
+global.group1 = 'Teri Maiya Ka Bharosaa😂🫶'
+global.group2 = 'CHALA JA BHOSDIKE😂'
+global.packname = 'ɪɴᴄ|🆁︎🆄︎🅿︎❤'
+global.ytchannel = '🫶'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -53,7 +53,7 @@ global.emeraldAwal = '5'
 global.umpanAwal = '5'
 global.potionAwal = '1'
 global.dripstyping = false
-global.dripsreadgroup = false
+global.dripsreadgroup = true
 global.dripsreadall = false
 global.dripsrecord = false
 global.available = false
@@ -65,7 +65,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = '❥͜͡𝄟♥⃝•⩤⋆🆁︎🅤︎🅿︎⋆⩥࿐ཽ༵•❥͜͡★'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
